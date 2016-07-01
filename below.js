bfcd6f7281f8ea21232b0dc20ba2ad1874e9a784
Shou <x@shou.io>
2016-07-01 21:44:23 +0100

    no @s in names

--- below.js
+++ below.js
@@ -430,6 +430,7 @@ function checkMention(e) {
       }
 
       var name = subnames.slice(0, index).join(' ').replace(/^@/, "")
+      console.log(subnames)
       console.log(name)
       var baseurl = "http://w11.zetaboards.com/bnetmlp/tasks/?mode=1&task=7&name="
       xhr.open("GET", baseurl + encodeURIComponent(name).replace(/%20/g, '+'))
